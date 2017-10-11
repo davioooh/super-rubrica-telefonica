687291901d91e7592f2d47ba24a72e8180d3e867
David <davidcastelletti@yahoo.it>
2017-10-11 12:10:34 +0200

    Spring Boot init

---
+++ src/main/java/com/davioooh/srt/SuperRubricaTelefonicaApplication.java
@@ -0,0 +1,12 @@
+package com.davioooh.srt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperRubricaTelefonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperRubricaTelefonicaApplication.class, args);
+	}
+}
