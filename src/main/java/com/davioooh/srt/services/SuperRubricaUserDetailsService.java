1983700be40711064ff9b6ae95967c8158ed5a1f
David <davidcastelletti@yahoo.it>
2017-11-24 12:09:07 +0100

    Correzioni

--- src/main/java/com/davioooh/srt/services/SuperRubricaUserDetailsService.java
+++ src/main/java/com/davioooh/srt/services/SuperRubricaUserDetailsService.java
@@ -17,7 +17,6 @@ public class SuperRubricaUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
